23c3f7a030e1aedd3c7a7e33d097821954c25d72
luca_dell_laptop <igor_mio@hotmail.it>
2020-01-27 16:46:33 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='marthe-tf',
+    version='',
+    packages=['marthe', 'marthe.old'],
+    url='',
+    license='',
+    author='lfranceschi',
+    author_email='',
+    description=''
+)
